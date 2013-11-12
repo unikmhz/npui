fbfcdad3a91c0de71a9e22fa97875efa127e9c2a
Andrey Gontchar <gontchar@gmail.com>
2013-11-12 07:15:39 +0000

    * [useraccount] Added a new user account module, a module that will provide an interface to user account information

---
+++ netprofile_useraccount/netprofile_useraccount/__init__.py
@@ -0,0 +1,28 @@
+from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.session import UnencryptedCookieSessionFactoryConfig
+from pyramid.mako_templating import renderer_factory as mako_factory
+
+from .models import (
+    DBSession,
+    Base,
+    )
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    authn_policy = AuthTktAuthenticationPolicy('secret')
+    my_session_factory = UnencryptedCookieSessionFactoryConfig('anothersecret')
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+    config = Configurator(settings=settings, session_factory=my_session_factory, authentication_policy=authn_policy)
+    config.add_renderer('.mako', mako_factory)
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.add_route('login', '/login')
+    config.add_route('logout', '/logout')
+    config.scan()
+    return config.make_wsgi_app()
