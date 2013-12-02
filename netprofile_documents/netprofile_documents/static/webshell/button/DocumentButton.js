823e213fcec9b03d8bf0823a0a37b0109715b10c
Alex Unigovsky <unik@compot.ru>
2013-12-02 02:23:38 +0000

    * [documents] Added documents module.

---
+++ netprofile_documents/netprofile_documents/static/webshell/button/DocumentButton.js
@@ -0,0 +1,77 @@
+/**
+ * @class NetProfile.documents.button.DocumentButton
+ * @extends Ext.button.Button
+ */
+Ext.define('NetProfile.documents.button.DocumentButton', {
+	extend: 'Ext.button.Button',
+	alias: 'widget.docbutton',
+	requires: [
+		'Ext.XTemplate'
+	],
+
+	objectType: null,
+
+	handler: function()
+	{
+		var me = this,
+			gen_panel = me.up('panel'),
+			rec_panel = gen_panel.up('panel[cls~=record-tab]'),
+			doc_box, obj_id, doc_id;
+
+		if(rec_panel && rec_panel.record)
+			obj_id = rec_panel.record.getId();
+		doc_box = gen_panel.getComponent('docid');
+		if(doc_box)
+			doc_id = parseInt(doc_box.getValue());
+		if(!obj_id || !doc_id)
+			return;
+		NetProfile.api.Document.prepare_template({
+			'objid'   : obj_id,
+			'objtype' : me.objectType,
+			'docid'   : doc_id
+		}, me.onPrepareTemplate, me);
+	},
+
+	onPrepareTemplate: function(data, res)
+	{
+		var doc, body, tpl, win, el;
+
+		if(!res.result || !res.result.success)
+			return;
+		doc = data.doc;
+		body = doc.body;
+		if(doc.type == 'html-ext')
+		{
+			tpl = new Ext.XTemplate(body);
+			body = tpl.apply(data.vars);
+		}
+		if(body && Ext.Array.contains(['html-plain', 'html-ext'], doc.type))
+		{
+			win = window.open(
+				'', 'doc_print',
+				'menubar=no,location=no,resizable=yes,scrollbars=yes,status=yes'
+			);
+			if(!win)
+				return; // TODO: alert user about blocked window
+			win.document.write('\074!DOCTYPE html\076\
+\074html xmlns="http://www.w3.org/1999/xhtml"\076\
+\074head\076\
+\074meta charset="UTF-8"\076\
+\074meta http-equiv="X-UA-Compatible" content="IE=edge;chrome=1" /\076\
+\074title\076' + doc.name + '\074/title\076\
+\074/head\076\
+\074body\076\
+\074/body\076\
+\074/html\076\
+			');
+			el = win.document.getElementsByTagName('body');
+			if(el && el.length)
+			{
+				el[0].innerHTML = body;
+				win.focus();
+				win.print();
+			}
+		}
+	}
+});
+
---
+++ netprofile_documents/netprofile_documents/views.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+#
+# NetProfile: Documents module - Views
+# © Copyright 2013 Alex 'Unik' Unigovsky
+#
+# This file is part of NetProfile.
+# NetProfile is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later
+# version.
+#
+# NetProfile is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General
+# Public License along with NetProfile. If not, see
+# <http://www.gnu.org/licenses/>.
+
+from __future__ import (
+	unicode_literals,
+	print_function,
+	absolute_import,
+	division
+)
+
+from pyramid.view import (
+	view_config
+)
+from pyramid.i18n import (
+	TranslationStringFactory,
+	get_localizer
+)
+from pyramid.security import has_permission
+from netprofile.common.hooks import register_hook
+from netprofile.db.connection import DBSession
+from netprofile.ext.direct import extdirect_method
+
+from .models import (
+	Document,
+	DocumentType
+)
+
+_ = TranslationStringFactory('netprofile_documents')
+
+@register_hook('core.dpane.entities.Entity')
+@register_hook('core.dpane.entities.PhysicalEntity')
+@register_hook('core.dpane.entities.LegalEntity')
+@register_hook('core.dpane.entities.StructuralEntity')
+@register_hook('core.dpane.entities.ExternalEntity')
+def _dpane_make_doc(cont, model, req):
+	if not has_permission('DOCUMENTS_GENERATE', req.context, req):
+		return
+	loc = get_localizer(req)
+	panel = {
+		'xtype'  : 'panel',
+		'title'  : loc.translate(_('Generate Documents')),
+		'layout' : {
+			'type' : 'hbox'
+		},
+		'defaults' : {
+			'margin' : 4
+		},
+		'border' : 0,
+		'items'  : [{
+			'xtype'          : 'combobox',
+			'itemId'         : 'docid',
+			'format'         : 'string',
+			'displayField'   : 'name',
+			'valueField'     : 'docid',
+			'queryMode'      : 'local',
+			'editable'       : False,
+			'forceSelection' : False,
+			'store'          : { 'type' : 'documents_Document' },
+			'flex'           : 1,
+			'border'         : 0,
+			'emptyText'      : loc.translate(_('Choose document template…'))
+		}, {
+			'xtype'      : 'docbutton',
+			'text'       : loc.translate(_('Generate')),
+			'iconCls'    : 'ico-print',
+			'objectType' : 'entity'
+		}]
+	}
+	cont['items'][0]['items'].append(panel)
+
+@extdirect_method('Document', 'prepare_template', request_as_last_param=True, permission='DOCUMENTS_GENERATE')
+def _dyn_prep_tpl(params, req):
+	objid = params.get('objid')
+	objtype = params.get('objtype')
+	if (not objid) or (not objtype):
+		raise ValueError('No object specified')
+	docid = params.get('docid')
+	if not docid:
+		raise ValueError('No document specified')
+
+	sess = DBSession()
+	docid = int(docid)
+	doc = sess.query(Document).get(docid)
+	if not doc:
+		raise KeyError('No such document')
+
+	config = {
+		'docid' : docid,
+		'code'  : doc.code,
+		'name'  : doc.name,
+		'type'  : doc.type,
+		'vars'  : doc.variables,
+		'body'  : doc.body
+	}
+	tpl_vars = {}
+	form_fields = []
+	req.run_hook('documents.gen.object', tpl_vars, objid, objtype, req)
+	req.run_hook('documents.gen.variables', tpl_vars, doc.variables, req)
+
+	return {
+		'success' : True,
+		'doc'     : config,
+		'vars'    : tpl_vars,
+		'form'    : form_fields
+	}
+
