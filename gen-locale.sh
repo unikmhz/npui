7aabd8916853ef2001b8b75a3a4cc6a5a7e898c1
Alex Unigovsky <unik@compot.ru>
2014-07-30 13:00:39 +0400

    Another i18n update
    Mostly cosmetic this time. The only fix is inclusion of 'netprofile'
    module when invoking gen-locale.sh script.

--- gen-locale.sh
+++ gen-locale.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-for moddef in `ls -1 -d netprofile_*`
+curdir=`pwd`
+for moddef in `find . -maxdepth 1 -type d -name 'netprofile*'`
 do
 	cd $moddef
 	python setup.py extract_messages
 	python setup.py update_catalog
 	python setup.py compile_catalog
-	cd -
+	cd $curdir
 done
 
