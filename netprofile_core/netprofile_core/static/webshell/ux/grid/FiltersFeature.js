5586de227ed246bcc1e87c05db9648c4cbc093b5
Alex Unigovsky <unik@compot.ru>
2015-03-07 16:35:46 +0300

    Added IPv6 grid filters

--- netprofile_core/netprofile_core/static/webshell/ux/grid/FiltersFeature.js
+++ netprofile_core/netprofile_core/static/webshell/ux/grid/FiltersFeature.js
@@ -103,7 +103,8 @@ Ext.define('Ext.ux.grid.FiltersFeature', {
         'Ext.ux.grid.filter.ListFilter',
         'Ext.ux.grid.filter.NumericFilter',
         'Ext.ux.grid.filter.StringFilter',
-		'Ext.ux.grid.filter.IPv4Filter'
+		'Ext.ux.grid.filter.IPv4Filter',
+		'Ext.ux.grid.filter.IPv6Filter'
     ],
 
     /**
@@ -786,9 +787,7 @@ Ext.define('Ext.ux.grid.FiltersFeature', {
               type = 'boolean';
               break;
             case 'ipv4':
-              break;
             case 'ipv6':
-              type = 'string';
               break;
         }
         return Ext.ClassManager.getByAlias('gridfilter.' + type);
