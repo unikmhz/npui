4dd9c2f06c24163f1d70cde6d901bd599a3acbcc
Alex Unigovsky <unik@compot.ru>
2016-02-19 06:31:31 +0300

    Improve string representation for places

--- netprofile_geo/netprofile_geo/models.py
+++ netprofile_geo/netprofile_geo/models.py
@@ -750,7 +750,13 @@ class Place(Base):
 	)
 
 	def __str__(self):
-		return '%s' % str(self.number)
+		if self.number:
+			fmt = _('#%s')
+			req = getattr(self, '__req__', None)
+			if req:
+				fmt = req.localizer.translate(fmt)
+			return fmt % (str(self.number),)
+		return str(self.name)
 
 class HouseGroup(Base):
 	"""
