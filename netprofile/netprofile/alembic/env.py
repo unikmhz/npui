8e13c355afe20916c65637c7f8aafa8dc195624a
Alex Unigovsky <unik@compot.ru>
2016-03-01 16:18:39 +0300

    Unify style

--- netprofile/netprofile/alembic/env.py
+++ netprofile/netprofile/alembic/env.py
@@ -181,58 +181,59 @@ def render_item(type_, obj, autogen_context):
 
 
 def run_migrations_offline():
-    """Run migrations in 'offline' mode.
+	"""Run migrations in 'offline' mode.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+	This configures the context with just a URL
+	and not an Engine, though an Engine is acceptable
+	here as well.  By skipping the Engine creation
+	we don't even need a DBAPI to be available.
 
-    Calls to context.execute() here emit the given string to the
-    script output.
+	Calls to context.execute() here emit the given string to the
+	script output.
 
-    """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=DBMeta,
-        literal_binds=True,
-        process_revision_directives=writer
-    )
+	"""
+	url = config.get_main_option("sqlalchemy.url")
+	context.configure(
+		url=url,
+		target_metadata=DBMeta,
+		literal_binds=True,
+		process_revision_directives=writer
+	)
 
-    with context.begin_transaction():
-        context.run_migrations()
+	with context.begin_transaction():
+		context.run_migrations()
 
 
 def run_migrations_online():
-    """Run migrations in 'online' mode.
+	"""Run migrations in 'online' mode.
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
+	In this scenario we need to create an Engine
+	and associate a connection with the context.
 
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix='sqlalchemy.',
-        poolclass=pool.NullPool)
+	"""
+	connectable = engine_from_config(
+		config.get_section(config.config_ini_section),
+		prefix='sqlalchemy.',
+		poolclass=pool.NullPool
+	)
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=DBMeta,
-            include_object=_include_object,
-            render_item=render_item,
-            compare_type=True,
-            compare_server_default=_compare_default,
-            process_revision_directives=writer
-        )
+	with connectable.connect() as connection:
+		context.configure(
+			connection=connection,
+			target_metadata=DBMeta,
+			include_object=_include_object,
+			render_item=render_item,
+			compare_type=True,
+			compare_server_default=_compare_default,
+			process_revision_directives=writer
+		)
 
-        with context.begin_transaction():
-            context.run_migrations()
+		with context.begin_transaction():
+			context.run_migrations()
 
 
 if context.is_offline_mode():
-    run_migrations_offline()
+	run_migrations_offline()
 else:
-    run_migrations_online()
+	run_migrations_online()
 
