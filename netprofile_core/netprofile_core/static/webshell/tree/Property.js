9e3ca38258be6f23d7f901b4bfd3163aeadd5b22
Alex Unigovsky <unik@compot.ru>
2016-03-17 08:18:36 +0300

    Add columns to prop tree

--- netprofile_core/netprofile_core/static/webshell/tree/Property.js
+++ netprofile_core/netprofile_core/static/webshell/tree/Property.js
@@ -6,12 +6,32 @@ Ext.define('NetProfile.tree.Property', {
 	extend: 'Ext.tree.Panel',
 	alias: 'widget.propertytree',
 	requires: [
+		'Ext.grid.column.Column',
+		'Ext.tree.Column',
 		'NetProfile.data.PropertyTreeStore'
 	],
+
+	propText: 'Property',
+	typeText: 'Type',
+	valueText: 'Value',
 
 	initComponent: function()
 	{
 		var me = this;
+
+		me.columns = [{
+			xtype: 'treecolumn',
+			text: me.propText,
+			dataIndex: 'name'
+		}, {
+			xtype: 'gridcolumn',
+			text: me.typeText,
+			dataIndex: 'type'
+		}, {
+			xtype: 'gridcolumn',
+			text: me.valueText,
+			dataIndex: 'value'
+		}];
 
 		me.callParent();
 	}
