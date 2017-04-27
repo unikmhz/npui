fa70c464765254e9f4239e3e5d4ce8514609eb24
Alex Unigovsky <unik@itws.ru>
2017-04-27 09:30:42 +0300

    Implement custom loader for celery 4

--- netprofile/netprofile/celery.py
+++ netprofile/netprofile/celery.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 #
 # NetProfile: Celery application setup
-# © Copyright 2014-2016 Alex 'Unik' Unigovsky
+# © Copyright 2014-2017 Alex 'Unik' Unigovsky
 #
 # This file is part of NetProfile.
 # NetProfile is free software: you can redistribute it and/or
@@ -28,7 +28,6 @@ from __future__ import (
 )
 
 import os
-import pkg_resources
 import transaction
 
 from pyramid.config import aslist
@@ -36,12 +35,11 @@ from pyramid.paster import (
 	get_appsettings,
 	setup_logging
 )
-from celery import Celery
-from celery.signals import (
-	beat_init,
-	celeryd_init,
-	worker_process_init
+from celery import (
+	Celery,
+	signals
 )
+from celery.loaders.base import BaseLoader
 from kombu import (
 	Exchange,
 	Queue
@@ -59,7 +57,10 @@ from netprofile.common.util import (
 	as_dict,
 	make_config_dict
 )
-from netprofile.db.clauses import SetVariable
+from netprofile.db.clauses import (
+	SetVariable,
+	SetVariables
+)
 from netprofile.db.connection import DBSession
 
 _default_queues = (
@@ -67,269 +68,76 @@ _default_queues = (
 	Broadcast('broadcast')
 )
 
-def _parse_ini_settings(reg, celery):
+def _parse_ini_settings(reg):
 	settings = reg.settings
 	cfg = make_config_dict(settings, 'celery.')
-	newconf = {}
-
-	if 'broker' in cfg:
-		newconf['BROKER_URL'] = cfg['broker']
-	if 'broker.heartbeat' in cfg:
-		newconf['BROKER_HEARTBEAT'] = cfg['broker.heartbeat']
-	if 'broker.heartbeat.checkrate' in cfg:
-		newconf['BROKER_HEARTBEAT_CHECKRATE'] = cfg['broker.heartbeat.checkrate']
-	if 'broker.use_ssl' in cfg:
-		newconf['BROKER_USE_SSL'] = cfg['broker.use_ssl']
-	if 'broker.pool_limit' in cfg:
-		newconf['BROKER_POOL_LIMIT'] = cfg['broker.pool_limit']
-	if 'broker.connection.timeout' in cfg:
-		newconf['BROKER_CONNECTION_TIMEOUT'] = cfg['broker.connection.timeout']
-	if 'broker.connection.retry' in cfg:
-		newconf['BROKER_CONNECTION_RETRY'] = cfg['broker.connection.retry']
-	if 'broker.connection.max_retries' in cfg:
-		newconf['BROKER_CONNECTION_MAX_RETRIES'] = cfg['broker.connection.max_retries']
-	if 'broker.login_method' in cfg:
-		newconf['BROKER_LOGIN_METHOD'] = cfg['broker.login_method']
-
-	if 'backend' in cfg:
-		newconf['CELERY_RESULT_BACKEND'] = cfg['backend']
-	if 'task_result_expires' in cfg:
-		newconf['CELERY_TASK_RESULT_EXPIRES'] = cfg['task_result_expires']
-	if 'task_publish_retry' in cfg:
-		newconf['CELERY_TASK_PUBLISH_RETRY'] = cfg['task_publish_retry']
-	if 'enable_utc' in cfg:
-		newconf['CELERY_ENABLE_UTC'] = cfg['enable_utc']
-	if 'timezone' in cfg:
-		newconf['CELERY_TIMEZONE'] = cfg['timezone']
-	if 'always_eager' in cfg:
-		newconf['CELERY_ALWAYS_EAGER'] = cfg['always_eager']
-	if 'eager_propagates_exceptions' in cfg:
-		newconf['CELERY_EAGER_PROPAGATES_EXCEPTIONS'] = cfg['eager_propagates_exceptions']
-	if 'ignore_result' in cfg:
-		newconf['CELERY_IGNORE_RESULT'] = cfg['ignore_result']
-	if 'store_errors_even_if_ignored' in cfg:
-		newconf['CELERY_STORE_ERRORS_EVEN_IF_IGNORED'] = cfg['store_errors_even_if_ignored']
-	if 'message_compression' in cfg:
-		newconf['CELERY_MESSAGE_COMPRESSION'] = cfg['message_compression']
-	if 'max_cached_results' in cfg:
-		newconf['CELERY_MAX_CACHED_RESULTS'] = cfg['max_cached_results']
-	if 'chord_propagates' in cfg:
-		newconf['CELERY_CHORD_PROPAGATES'] = cfg['chord_propagates']
-	if 'track_started' in cfg:
-		newconf['CELERY_TRACK_STARTED'] = cfg['track_started']
-	if 'default_rate_limit' in cfg:
-		newconf['CELERY_DEFAULT_RATE_LIMIT'] = cfg['default_rate_limit']
-	if 'disable_rate_limits' in cfg:
-		newconf['CELERY_DISABLE_RATE_LIMITS'] = cfg['disable_rate_limits']
-	if 'acks_late' in cfg:
-		newconf['CELERY_ACKS_LATE'] = cfg['acks_late']
 
 	if 'accept_content' in cfg:
-		newconf['CELERY_ACCEPT_CONTENT'] = aslist(cfg['accept_content'])
+		cfg['accept_content'] = aslist(cfg['accept_content'])
 	else:
-		newconf['CELERY_ACCEPT_CONTENT'] = ('msgpack',)
+		cfg['accept_content'] = ('msgpack',)
 
-	if 'task_serializer' in cfg:
-		newconf['CELERY_TASK_SERIALIZER'] = cfg['task_serializer']
-	else:
-		newconf['CELERY_TASK_SERIALIZER'] = 'msgpack'
-	if 'result_serializer' in cfg:
-		newconf['CELERY_RESULT_SERIALIZER'] = cfg['result_serializer']
-	else:
-		newconf['CELERY_RESULT_SERIALIZER'] = 'msgpack'
-	if 'event_serializer' in cfg:
-		newconf['CELERY_EVENT_SERIALIZER'] = cfg['event_serializer']
-	else:
-		newconf['CELERY_EVENT_SERIALIZER'] = 'msgpack'
+	if 'task_serializer' not in cfg:
+		cfg['task_serializer'] = 'msgpack'
+	if 'result_serializer' not in cfg:
+		cfg['result_serializer'] = 'msgpack'
+	if 'event_serializer' not in cfg:
+		cfg['event_serializer'] = 'msgpack'
 
-	if 'result_exchange' in cfg:
-		newconf['CELERY_RESULT_EXCHANGE'] = cfg['result_exchange']
-	if 'result_exchange_type' in cfg:
-		newconf['CELERY_RESULT_EXCHANGE_TYPE'] = cfg['result_exchange_type']
-	if 'result_persistent' in cfg:
-		newconf['CELERY_RESULT_PERSISTENT'] = cfg['result_persistent']
-	if 'routes' in cfg:
-		newconf['CELERY_ROUTES'] = aslist(cfg['routes'])
-	if 'worker_direct' in cfg:
-		newconf['CELERY_WORKER_DIRECT'] = cfg['worker_direct']
-	if 'create_missing_queues' in cfg:
-		newconf['CELERY_CREATE_MISSING_QUEUES'] = cfg['create_missing_queues']
-	if 'enable_remote_control' in cfg:
-		newconf['CELERY_ENABLE_REMOTE_CONTROL'] = cfg['enable_remote_control']
+	if 'task_routes' in cfg:
+		cfg['task_routes'] = aslist(cfg['task_routes'])
 
-	if 'send_task_error_emails' in cfg:
-		newconf['CELERY_SEND_TASK_ERROR_EMAILS'] = cfg['send_task_error_emails']
-#	if 'admins' in cfg:
-#		FIXME: list of tuples
-	if 'server_email' in cfg:
-		newconf['SERVER_EMAIL'] = cfg['server_email']
-	if 'email_host' in cfg:
-		newconf['EMAIL_HOST'] = cfg['email_host']
-	if 'email_host_user' in cfg:
-		newconf['EMAIL_HOST_USER'] = cfg['email_host_user']
-	if 'email_host_password' in cfg:
-		newconf['EMAIL_HOST_PASSWORD'] = cfg['email_host_password']
-	if 'email_port' in cfg:
-		newconf['EMAIL_PORT'] = cfg['email_port']
-	if 'email_use_ssl' in cfg:
-		newconf['EMAIL_USE_SSL'] = cfg['email_use_ssl']
-	if 'email_use_tls' in cfg:
-		newconf['EMAIL_USE_TLS'] = cfg['email_use_tls']
-	if 'email_timeout' in cfg:
-		newconf['EMAIL_TIMEOUT'] = cfg['email_timeout']
-
-	if 'send_events' in cfg:
-		newconf['CELERY_SEND_EVENTS'] = cfg['send_events']
-	if 'send_task_sent_event' in cfg:
-		newconf['CELERY_SEND_TASK_SENT_EVENT'] = cfg['send_task_sent_event']
-	if 'event_queue_ttl' in cfg:
-		newconf['CELERY_EVENT_QUEUE_TTL'] = cfg['event_queue_ttl']
-	if 'event_queue_expires' in cfg:
-		newconf['CELERY_EVENT_QUEUE_EXPIRES'] = cfg['event_queue_expires']
-	if 'redirect_stdouts' in cfg:
-		newconf['CELERY_REDIRECT_STDOUTS'] = cfg['redirect_stdouts']
-	if 'redirect_stdouts_level' in cfg:
-		newconf['CELERY_REDIRECT_STDOUTS_LEVEL'] = cfg['redirect_stdouts_level']
-
-	if 'queue_ha_policy' in cfg:
-		qhp = aslist(cfg['queue_ha_policy'])
+	if 'task_queue_ha_policy' in cfg:
+		qhp = aslist(cfg['task_queue_ha_policy'])
 		if len(qhp) == 1:
 			qhp = qhp[0]
-		newconf['CELERY_QUEUE_HA_POLICY'] = qhp
+		cfg['task_queue_ha_policy'] = qhp
 
-	if 'security_key' in cfg:
-		newconf['CELERY_SECURITY_KEY'] = cfg['security_key']
-	if 'security_certificate' in cfg:
-		newconf['CELERY_SECURITY_CERTIFICATE'] = cfg['security_certificate']
-	if 'security_cert_store' in cfg:
-		newconf['CELERY_SECURITY_CERT_STORE'] = cfg['security_cert_store']
-
-	if 'default_queue' in cfg:
-		newconf['CELERY_DEFAULT_QUEUE'] = cfg['default_queue']
-	if 'default_exchange' in cfg:
-		newconf['CELERY_DEFAULT_EXCHANGE'] = cfg['default_exchange']
-	if 'default_exchange_type' in cfg:
-		newconf['CELERY_DEFAULT_EXCHANGE_TYPE'] = cfg['default_exchange_type']
-	if 'default_routing_key' in cfg:
-		newconf['CELERY_DEFAULT_ROUTING_KEY'] = cfg['default_routing_key']
-	if 'default_delivery_mode' in cfg:
-		newconf['CELERY_DEFAULT_DELIVERY_MODE'] = cfg['default_delivery_mode']
-
-	if 'concurrency' in cfg:
-		newconf['CELERYD_CONCURRENCY'] = cfg['concurrency']
-	if 'prefetch_multiplier' in cfg:
-		newconf['CELERYD_PREFETCH_MULTIPLIER'] = cfg['prefetch_multiplier']
-	if 'force_execv' in cfg:
-		newconf['CELERYD_FORCE_EXECV'] = cfg['force_execv']
-	if 'worker_lost_wait' in cfg:
-		newconf['CELERYD_WORKER_LOST_WAIT'] = cfg['worker_lost_wait']
-	if 'max_tasks_per_child' in cfg:
-		newconf['CELERYD_MAX_TASKS_PER_CHILD'] = cfg['max_tasks_per_child']
-	if 'task_time_limit' in cfg:
-		newconf['CELERYD_TASK_TIME_LIMIT'] = cfg['task_time_limit']
-	if 'task_soft_time_limit' in cfg:
-		newconf['CELERYD_TASK_SOFT_TIME_LIMIT'] = cfg['task_soft_time_limit']
-	if 'state_db' in cfg:
-		newconf['CELERYD_STATE_DB'] = cfg['state_db']
-	if 'timer_precision' in cfg:
-		newconf['CELERYD_TIMER_PRECISION'] = cfg['timer_precision']
-	if 'hijack_root_logger' in cfg:
-		newconf['CELERYD_HIJACK_ROOT_LOGGER'] = cfg['hijack_root_logger']
-	if 'log_color' in cfg:
-		newconf['CELERYD_LOG_COLOR'] = cfg['log_color']
-	if 'log_format' in cfg:
-		newconf['CELERYD_LOG_FORMAT'] = cfg['log_format']
-	if 'task_log_format' in cfg:
-		newconf['CELERYD_TASK_LOG_FORMAT'] = cfg['task_log_format']
-	if 'pool' in cfg:
-		newconf['CELERYD_POOL'] = cfg['pool']
-	if 'pool_restarts' in cfg:
-		newconf['CELERYD_POOL_RESTARTS'] = cfg['pool_restarts']
-	if 'autoscaler' in cfg:
-		newconf['CELERYD_AUTOSCALER'] = cfg['autoscaler']
-	if 'autoreloader' in cfg:
-		newconf['CELERYD_AUTORELOADER'] = cfg['autoreloader']
-	if 'consumer' in cfg:
-		newconf['CELERYD_CONSUMER'] = cfg['consumer']
-	if 'timer' in cfg:
-		newconf['CELERYD_TIMER'] = cfg['timer']
-
-	if 'celerymon_log_format' in cfg:
-		newconf['CELERYMON_LOG_FORMAT'] = cfg['celerymon_log_format']
-
-	if 'broadcast_queue' in cfg:
-		newconf['CELERY_BROADCAST_QUEUE'] = cfg['broadcast_queue']
-	if 'broadcast_exchange' in cfg:
-		newconf['CELERY_BROADCAST_EXCHANGE'] = cfg['broadcast_exchange']
-	if 'broadcast_exchange_type' in cfg:
-		newconf['CELERY_BROADCAST_EXCHANGE_TYPE'] = cfg['broadcast_exchange_type']
-
-	if 'scheduler' in cfg:
-		newconf['CELERYBEAT_SCHEDULER'] = cfg['scheduler']
-	else:
+	if 'beat_scheduler' not in cfg:
 		# FIXME: strip scheduler parts out of 'core' module
-		newconf['CELERYBEAT_SCHEDULER'] = 'netprofile_core.celery:Scheduler'
-	if 'schedule_filename' in cfg:
-		newconf['CELERYBEAT_SCHEDULE_FILENAME'] = cfg['schedule_filename']
-	if 'sync_every' in cfg:
-		newconf['CELERYBEAT_SYNC_EVERY'] = cfg['sync_every']
-	if 'max_loop_interval' in cfg:
-		newconf['CELERYBEAT_MAX_LOOP_INTERVAL'] = cfg['max_loop_interval']
+		cfg['beat_scheduler'] = 'netprofile_core.celery:Scheduler'
 
 	# FIXME: complex python values!
-	opts = make_config_dict(cfg, 'schedule.')
+	opts = make_config_dict(cfg, 'beat_schedule.')
 	if len(opts) > 0:
-		newconf['CELERYBEAT_SCHEDULE'] = as_dict(opts)
+		cfg['beat_schedule'] = as_dict(opts)
 
-	if 'redis_max_connections' in cfg:
-		newconf['CELERY_REDIS_MAX_CONNECTIONS'] = cfg['redis_max_connections']
-
-	opts = make_config_dict(cfg, 'broker.transport_options.')
+	opts = make_config_dict(cfg, 'broker_transport_options.')
 	if len(opts) > 0:
-		newconf['BROKER_TRANSPORT_OPTIONS'] = opts
+		cfg['broker_transport_options'] = opts
 
 	opts = make_config_dict(cfg, 'task_publish_retry_policy.')
 	if len(opts) > 0:
-		newconf['CELERY_TASK_PUBLISH_RETRY_POLICY'] = opts
+		cfg['task_publish_retry_policy'] = opts
 
-	opts = make_config_dict(cfg, 'result_tables.')
+	opts = make_config_dict(cfg, 'database_db_names.')
 	if len(opts) > 0:
-		newconf['CELERY_RESULT_DB_TABLENAMES'] = opts
+		cfg['database_db_names'] = opts
 
-	opts = make_config_dict(cfg, 'result_options.')
+	opts = make_config_dict(cfg, 'database_engine_options.')
 	if len(opts) > 0:
-		newconf['CELERY_RESULT_ENGINE_OPTIONS'] = opts
+		cfg['database_engine_options'] = opts
 
-	opts = make_config_dict(cfg, 'cache_options.')
+	opts = make_config_dict(cfg, 'cache_backend_options.')
 	if len(opts) > 0:
-		newconf['CELERY_CACHE_BACKEND_OPTIONS'] = opts
+		cfg['cache_backend_options'] = opts
 
-	opts = make_config_dict(cfg, 'mongodb_options.')
+	opts = make_config_dict(cfg, 'mongodb_backend_settings.')
 	if len(opts) > 0:
-		newconf['CELERY_MONGODB_BACKEND_SETTINGS'] = opts
+		cfg['mongodb_backend_settings'] = opts
 
-	cass = make_config_dict(cfg, 'cassandra.')
-	if 'servers' in cass:
-		newconf['CASSANDRA_SERVERS'] = aslist(cass['servers'])
-	if 'keyspace' in cass:
-		newconf['CASSANDRA_KEYSPACE'] = cass['keyspace']
-	if 'column_family' in cass:
-		newconf['CASSANDRA_COLUMN_FAMILY'] = cass['column_family']
-	if 'read_consistency' in cass:
-		newconf['CASSANDRA_READ_CONSISTENCY'] = cass['read_consistency']
-	if 'write_consistency' in cass:
-		newconf['CASSANDRA_WRITE_CONSISTENCY'] = cass['write_consistency']
-	if 'detailed_mode' in cass:
-		newconf['CASSANDRA_DETAILED_MODE'] = cass['detailed_mode']
-	opts = make_config_dict(cass, 'options.')
-	if len(opts) > 0:
-		newconf['CASSANDRA_OPTIONS'] = opts
+	if 'cassandra_servers' in cfg:
+		cfg['cassandra_servers'] = aslist(cfg['cassandra_servers'])
 
-	opts = make_config_dict(cfg, 'queues.')
+	opts = make_config_dict(cfg, 'cassandra_auth_kwargs.')
 	if len(opts) > 0:
-		newconf['CELERY_QUEUES'] = opts
+		cfg['cassandra_auth_kwargs'] = as_dict(opts)
+
+	opts = make_config_dict(cfg, 'task_queues.')
+	if len(opts) > 0:
+		cfg['task_queues'] = opts
 	else:
-		newconf['CELERY_QUEUES'] = _default_queues
+		cfg['task_queues'] = _default_queues
 
 	mm = reg.getUtility(IModuleManager)
 
@@ -338,21 +146,42 @@ def _parse_ini_settings(reg, celery):
 		opts = aslist(cfg['imports'])
 	for imp in mm.get_task_imports():
 		opts.append(imp)
-	if len(opts) > 0:
-		newconf['CELERY_IMPORTS'] = opts
+	cfg['imports'] = opts
 
 	opts = []
 	if 'include' in cfg:
 		opts = aslist(cfg['include'])
 	# FIXME: hook module include here (?)
-	if len(opts) > 0:
-		newconf['CELERY_INCLUDE'] = opts
+	cfg['include'] = opts
 
-	if len(newconf) > 0:
-		if isinstance(celery, Celery):
-			celery.config_from_object(newconf)
-		else:
-			celery.update(newconf)
+	return cfg
+
+class AppLoader(BaseLoader):
+	def read_configuration(self, env='CELERY_CONFIG_MODULE'):
+		ini_file='production.ini'
+		if 'NP_INI_FILE' in os.environ:
+			ini_file = os.environ['NP_INI_FILE']
+
+		ini_name='netprofile'
+		if 'NP_INI_NAME' in os.environ:
+			ini_name = os.environ['NP_INI_NAME']
+
+		config_uri = '#'.join((ini_file, ini_name))
+		setup_logging(config_uri)
+		settings = get_appsettings(config_uri)
+
+		cfg = setup_config(settings)
+		cfg.commit()
+
+		mmgr = cfg.registry.getUtility(IModuleManager)
+		mmgr.load('core')
+		mmgr.load_enabled()
+
+		self.app.config = cfg
+		self.app.settings = settings
+		self.app.mmgr = mmgr
+
+		return _parse_ini_settings(cfg.registry)
 
 app = Celery('netprofile')
 
@@ -366,38 +195,8 @@ def task_meta(**kwargs):
 
 	return _app_task_wrapper
 
-@celeryd_init.connect
-def _setup(conf=None, **kwargs):
-	global app
-
-	ini_file='production.ini'
-	if 'NP_INI_FILE' in os.environ:
-		ini_file = os.environ['NP_INI_FILE']
-
-	ini_name='netprofile'
-	if 'NP_INI_NAME' in os.environ:
-		ini_name = os.environ['NP_INI_NAME']
-
-	config_uri = '#'.join((ini_file, ini_name))
-	setup_logging(config_uri)
-	settings = get_appsettings(config_uri)
-
-	cfg = setup_config(settings)
-	cfg.commit()
-
-	mmgr = cfg.registry.getUtility(IModuleManager)
-	mmgr.load('core')
-	mmgr.load_enabled()
-
-	_parse_ini_settings(cfg.registry, conf)
-	app.config = cfg
-	app.settings = settings
-	app.mmgr = mmgr
-
-	transaction.abort()
-	DBSession.remove()
-
 def _worker_setup(*args, **kwargs):
+	DBSession.remove()
 	engine = engine_from_config(
 		app.settings,
 		prefix='sqlalchemy.',
@@ -406,23 +205,31 @@ def _worker_setup(*args, **kwargs):
 	DBSession.configure(bind=engine)
 
 	def _after_begin(sess, trans, conn):
-		sess.execute(SetVariable('accessuid', 0))
-		sess.execute(SetVariable('accessgid', 0))
-		sess.execute(SetVariable('accesslogin', '[TASK]'))
+		try:
+			sess.execute(SetVariables(
+				accessuid=0,
+				accessgid=0,
+				accesslogin='[TASK]'
+			))
+		except NotImplementedError:
+			sess.execute(SetVariable('accessuid', 0))
+			sess.execute(SetVariable('accessgid', 0))
+			sess.execute(SetVariable('accesslogin', '[TASK]'))
 
 	event.listen(DBSession, 'after_begin', _after_begin)
 
-worker_process_init.connect(_worker_setup)
-beat_init.connect(_worker_setup)
+signals.worker_process_init.connect(_worker_setup)
+signals.beat_init.connect(_worker_setup)
 
 def setup_celery(reg):
-	_parse_ini_settings(reg, app)
+	celery_conf = _parse_ini_settings(reg)
+	app.config_from_object(celery_conf)
 
 def includeme(config):
 	"""
 	For inclusion by Pyramid.
 	"""
-	_parse_ini_settings(config.registry, app)
+	setup_celery(config.registry)
 
 if __name__ == '__main__':
 	app.start()
